7d5fa349bde565a804e7df3990381c927ea7df48
Giordon Stark <kratsg@gmail.com>
2022-09-28 15:41:55 -0700

    migrate to shell script instead

---
+++ html5.sh
@@ -0,0 +1,14 @@
+latexmlc \
+  --preload=[nobibtex,ids,localrawstyles,mathlexemes,magnify=2,zoomout=2,tokenlimit=99999999,iflimit=1499999,absorblimit=1299999,pushbacklimit=599999]latexml.sty \
+  --path=/opt/arxmliv-bindings/bindings \
+  --path=/opt/arxmliv-bindings/supported_originals \
+  --format=html5 \
+  --pmml \
+  --cmml \
+  --mathtex \
+  --timeout=2700 \
+  --nodefaultresources \
+  --css=https://cdn.jsdelivr.net/gh/dginev/ar5iv-css@0.7.4/css/ar5iv.min.css \
+  --source=main.tex \
+  --destination=html/main.html \
+  --javascript=main.js
