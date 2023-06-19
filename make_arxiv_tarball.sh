90832d3a6c08ad2c11e4e2a197855e8a66042960
Mike Hance <michael.hance@cern.ch>
2023-06-19 08:48:42 -0700

    getting script to handle cache correctly

--- make_arxiv_tarball.sh
+++ make_arxiv_tarball.sh
@@ -1,5 +1,10 @@
 rm -f mapyde_arxiv.tar.gz
 
+sed s/"finalizecache"/"frozencache"/ main.tex > main_final.tex
+
+cp -p main.tex main_backup.tex
+cp -p main_final.tex main.tex
+
 tar -cvf mapyde_arxiv.tar \
     main.tex \
     main.bbl \
@@ -13,3 +18,5 @@ tar -cvf mapyde_arxiv.tar \
     _minted-main/*
 
 gzip mapyde_arxiv.tar
+
+cp -p main_backup.tex main.tex
